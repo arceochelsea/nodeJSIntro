7b44e835ffe28e1a77bf9b81e1b94d4dbe47f859
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-07-25 17:50:27 -0400

    Function Expressions

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-setTimeout(function(){
-    console.log('3 seconds have passed');
-}, 3000);
+// //normal function statement
+function sayHi(){
+    console.log('hi');
+}
 
-//when this file is run in node set time out is gonna fire this function after 300 milliseconds which is 3 seconds and will log that message in the terminal there.
+sayHi();
 
-var time = 0;
+// //function expression
+var sayBye = function (){ //this is an anonymous function, setting it equal to a variable.
+console.log('bye');
+};
 
-setInterval(function(){ 
-    time += 2; //time = time + 2;
-    console.log(time + ' seconds have passed');
-}, 2000);
+sayBye();
 
-//every time this is called after 2000 miliseconds it will add 2
+//
 
-var time = 0;
+function callFunction (fun) {
+    fun();
+} 
 
-var timer = setInterval(function(){ 
-    time += 2; //time = time + 2;
-    console.log(time + ' seconds have passed');
-    if (time > 5) {
-        clearInterval(timer);
-    }
-}, 2000);
-
-//once its over the 5 seconds mark it will clear the Interval
-
-console.log(__dirname);
-
-//will print the directory youre in in the terminal
-
-console.log(__filename);
-
-//will print the dir and file youre in
+var sayBye = function (){ //this variable is gonna be passed to ^function above
+    console.log('bye');
+};
+    
+callFunction(sayBye);
 
