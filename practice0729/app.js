5e7cfb129e31f9b679d9c95d75c6d4502ed459bb
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-07-30 01:39:47 -0400

    Added some notes

--- practice0729/app.js
+++ practice0729/app.js
@@ -1,15 +1,14 @@
-const express = require('express'); 
+const express = require('express'); //exporting the module
 const mongoose = require('mongoose');
 
-const app = express();
+const app = express(); //firing the function which allows you to have access to different methods
 
 const port = 3000;
 
-app.get('/', function(req, res){
-    res.send('default')
+app.get('/', function(req, res){ //when user goes to this address (first param), its going to fire this call back function (second param)
+    res.send('default') //this will be sent to server
 })
 
-app.listen(port, function(req, res){
-    console.log(`Server is now listening on port ${port}`);
+app.listen(port, function(req, res){ //creates a listener on specified port / path 
+    console.log(`Server is now listening on port ${port}`); //sent to terminal
 });
-
