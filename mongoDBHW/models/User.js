4b986ea74b5df96cde31fc84567d57664a4d1fc1
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-08-16 23:28:09 -0400

    Added User Schema and Model

---
+++ mongoDBHW/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose'); //first we must require the package into file
+
+//next we must create schema 
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 30,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 100,
+        unique: true,
+    },
+    emailValidated: {
+        type: Boolean,
+        default: false
+    }
+}); 
+
+const UserModel = mongoose.model('user', UserSchema); // we are taking the UserSchema and compile it into a model which returns a bunch of methods. So it will take all the information from the schema and new info from the methods we will give it like user.find() etc.
+
+module.exports = UserModel; 
+
