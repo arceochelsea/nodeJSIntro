1c446b1b8640190060caf5d7dfbf3263feb382dc
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-08-17 23:31:55 -0400

    Added find method to userRouter

--- mongoDBHW/routes/homeRouter.js
+++ mongoDBHW/routes/homeRouter.js
@@ -6,20 +6,11 @@ const router = express.Router();
 
 router.get('/', function (req,res){
 
-    const filePath = process.cwd() + '/public.home.html';
+    const filePath = process.cwd() + '/public/home.html';
 
     console.log(filePath);
 
     res.sendFile(filePath);
 });
-
-// router.get('/login', function(req,res){
-    
-//     const filePath = process.cwd() + '/public.login.html';
-
-//     console.log(filePath);
-
-//     res.sendFile(filePath);
-// });
 
 module.exports = router;
--- mongoDBHW/routes/userRouter.js
+++ mongoDBHW/routes/userRouter.js
@@ -22,4 +22,57 @@ router.post('/register', async (req, res) => {
     }
 })
 
+//@path: LH/user/all
+//@desp: fetch all user data in the database, does not include sensitive info 
+//@access: public
+
+router.get('/all', async (req, res) => {
+
+    try {
+
+        const allUsers = await User.find({}) 
+
+        console.log(allUsers);
+
+        res.json(allUsers);
+
+    } catch (error) {
+
+        const msg = error.message || error;
+        console.error(msg)
+        res.status(500).json({
+            message: msg
+        })
+    }
+})
+
+//@path: LH/user/username/:username
+//@desp: fetch all user data in the data, do not include sensitive info 
+//@access: public
+
+router.get('/username/:username', async (req, res) => {
+
+    try {
+
+        const query = {username: req.params.username};
+
+        const projection = {email: 1, username: 1, _id: 0};
+
+        const oneUser = await User.findOne(query, projection);
+
+        res.json(oneUser);
+
+    } catch (error) {
+
+        const msg = error.message || err;
+        
+        console.log(msg);
+
+        res.status(500).json({
+            message: msg
+        })
+    }
+
+})
+
 module.exports = router;
