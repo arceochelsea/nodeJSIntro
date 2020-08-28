bd8fd913ccc2a652c4411c808a182c1a0ee5c34b
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-08-27 23:25:56 -0400

    created User Model

---
+++ favMovies/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 50,
+        unique: true,
+    },
+    id: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 6,
+        unique: true,
+    },
+    favMovie: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 100,
+        unique: false
+    }
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
