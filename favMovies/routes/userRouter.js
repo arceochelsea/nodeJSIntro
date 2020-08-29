44ca48704cde575ec14dc2f6aa6f12cbe680b10c
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-08-28 23:22:34 -0400

    userRouter get and post req

--- favMovies/routes/userRouter.js
+++ favMovies/routes/userRouter.js
@@ -1,3 +1,43 @@
 const express = require('express');
 
 const router = express.Router();
+
+//
+
+router.post('/profile', async (req, res) => {
+
+    console.log(req.body);
+
+    try {
+
+        await User.create(req.body);
+        
+        res.json({message: 'success!'})
+
+    } catch (error) {
+
+        res.status(500).json({
+
+            message: error.message
+
+        })
+    }
+})
+
+router.get('/all', async (req, res) => {
+    try {
+
+        const allUsers = await User.find({})
+
+        console.log(allUsers); 
+
+        res.json(allUser) //prints to client side
+    
+    } catch (error) {
+        const msg = error.message || error;
+        console.error(msg)
+        res.status(500).json({
+            message: msg
+        })
+    }
+})
